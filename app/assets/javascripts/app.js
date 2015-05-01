50a1a75abd8353e6a1b7728ec3531e2c0fcb7941
Isaac Saqib <isaacsaqib1@Isaacs-MBP.home>
2015-05-01 15:38:33 -0400

    making contact page and adjusting styling on quartz slabs

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 	$('#quartz-slabs').hide()
 	$('#quartz-header').hide();
+	$('#contact-div').hide();
+
 	// $('#box').hide()
 
 	$('#nav li:nth-child(2)').on('click', function(){
@@ -8,10 +10,28 @@ $(document).ready(function(){
 			$('body').css('background-image', 'none').css('background-color', 'white');
 			$('#quartz-slabs').fadeIn(2000)
 			$('#quartz-header').show(2000);
+			$('#contact-div').hide();
+
 
 
 
 	})
+
+	$('#nav li:nth-child(4)').on('click', function(){
+			$('#box').fadeOut(100)
+			$('body').css('background-image', 'none').css('background-color', 'white');
+			$('#contact-div').slideDown();
+				$('#quartz-slabs').hide()
+					$('#quartz-header').hide();
+
+
+			// $('#quartz-slabs').fadeIn(2000)
+			// $('#quartz-header').show(2000);
+
+
+
+	})
+
 
 
 })
